8e4968b64ae2c08496a5fbaaba6bb8d2dadb060b
Svencap <Ayzekmanduar@mail.ru>
2023-01-16 18:53:25 +0900

    add routes api

---
+++ router.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import Controller from "./controller.js";
+
+const controller = new Controller();
+const router = new Router();
+
+// router.get('/users', controller.getUsers);
+
+router.post('/users', controller.getUsers);
+
+router.post('/googleAuth', controller.googleAuth);
+
+router.post('/login', controller.login);
+
+
+export default router;  
